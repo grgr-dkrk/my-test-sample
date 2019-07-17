d618772dd659f34ebd78f099b78fdfa77a95f08c
grgr-dkrk <40130327+grgr-dkrk@users.noreply.github.com>
2019-07-17 09:59:39 +0900

    Initial commit

---
+++ src/spec/async.spec.ts
@@ -0,0 +1,26 @@
+type ResolveType = {
+  data: string;
+};
+
+// Promise
+const fetchData = (): Promise<ResolveType> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: 'foo',
+      });
+    }, 1000);
+  });
+};
+
+const rejected = (): Promise<never> => {
+  return Promise.reject(new Error('error'));
+};
+
+it('dataからfooが取得できているか', () => {
+  return expect(fetchData()).resolves.toEqual({ data: 'foo' });
+});
+
+it('rejectされるか', () => {
+  return expect(rejected()).rejects.toThrow('error');
+});
